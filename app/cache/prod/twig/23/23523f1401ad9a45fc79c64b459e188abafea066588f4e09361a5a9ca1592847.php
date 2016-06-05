e53b65d467d5aa33858c60f36530cfc598eae67a
Badr Eddine Sebbani <badre.808@gmail.com>
2016-06-05 19:21:43 +0200

    First Commit

---
+++ app/cache/prod/twig/23/23523f1401ad9a45fc79c64b459e188abafea066588f4e09361a5a9ca1592847.php
@@ -0,0 +1,34 @@
+<?php
+
+/* UserBundle:Default:index.html.twig */
+class __TwigTemplate_63ab90d52b4a287752fdd0e3083b2e64e44a3eeb8a2c0134e02c76ec39d34aea extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello World!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "UserBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* Hello World!*/
+/* */
